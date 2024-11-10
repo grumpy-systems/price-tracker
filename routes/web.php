bb5fa3189ce50eb038ce44cafbeca821ea2f8bfa
Grumpy <grumpy.systems@gmail.com>
2024-11-09 23:07:53 -0600

    Fix double ;;

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response('Coming soon', 200)
-        ->header('Content-Type', 'text/plain');;
+        ->header('Content-Type', 'text/plain');
 });
