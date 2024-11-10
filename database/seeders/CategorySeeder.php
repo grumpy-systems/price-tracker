8011c33ad2f386373fdf1b542339875d843353ce
Grumpy <grumpy.systems@gmail.com>
2024-11-10 11:22:26 -0600

    Add category seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Staples',
+            'Dairy',
+            'Pre-Packaged Food',
+            'Ramen',
+        ];
+
+        foreach ($categories as $category) {
+            DB::table('product_categories')->insert([
+                'name' => $category
+            ]);
+        }
+    }
+}
