139edf763f56e57ba9b7fad87ffe164a50a373b0
Grumpy <grumpy.systems@gmail.com>
2024-11-11 01:11:01 -0600

    Add link to about

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -13,7 +13,9 @@
         </div>
         <div class="overlay"></div>
         <footer>
-            &copy; 2024.  Photo by <a href="https://unsplash.com/@helloimnik?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nik</a> on <a href="https://unsplash.com/photos/brown-eggs-in-a-box-LUYD2b7MNrg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+            &copy; 2024. | 
+            Photo by <a href="https://unsplash.com/@helloimnik?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nik</a> on <a href="https://unsplash.com/photos/brown-eggs-in-a-box-LUYD2b7MNrg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a> |
+            <a href="/about">About</a>
         </footer>
     </div>
 @endsection
