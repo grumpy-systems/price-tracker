5f9e674931aa014659cdc6b13e97ceae4b7f404b
Grumpy <grumpy.systems@gmail.com>
2024-11-10 21:29:08 -0600

    Move Kroger API code to new class

---
+++ app/Kroger.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App;
+
+use App\Models\Product;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+use RuntimeException;
+
+class Kroger {
+    private $client;
+    private $token;
+
+    public function __construct($token = '')
+    {
+        $this->client = new Client([
+            'base_uri' => 'https://api.kroger.com/v1/',
+            'timeout'  => 2.0,
+        ]);
+
+        $this->token = $token;
+    }
+
+    /**
+     * Get a new authentication token.
+     */
+    public function GetAuthToken() {
+        Log::info('Getting new access token.');
+
+        $clientId = env('KROGER_CLIENT_ID');
+        $secret = env('KROGER_SECRET');
+
+        if (!$clientId || !$secret) {
+            throw new RuntimeException('missing kroger API credentials');
+        }
+
+        $resp = $this->client->post('connect/oauth2/token', [
+            'form_params' => [
+                'grant_type' => 'client_credentials',
+                'scope' => 'product.compact' // Locations has no scope, but products requires us to ask for this scope.
+            ],
+            'auth' => [
+                $clientId,
+                $secret
+            ]
+        ]);
+
+        $data = json_decode($resp->getBody());
+
+        return($data->access_token);
+    }
+
+    /**
+     * Search for a product at a location
+     */
+    public function SearchForProduct($query, $location) {
+        $resp = $this->client->get('products', [
+            'query' => [
+                'filter.term' => $query,
+                'filter.fulfillment' => 'ais', // Only get products in the store.
+                'filter.locationId' => $location,
+            ],
+            'headers' => [
+                'Authorization' => "Bearer " . $this->token,
+            ]
+        ]);
+
+        $data = json_decode($resp->getBody());
+
+        $out = [];
+
+        // Parse out the items into objects.  This makes the search a lot easier to print.
+        foreach ($data->data as $row) {
+            $image = '';
+            foreach ($row->images as $class) {
+                if (property_exists($class, 'featured') && $class->featured) {
+                    foreach ($class->sizes as $size) {
+                        if ($size->size == "large") {
+                            $image = $size->url;
+                        }
+                    }
+                }
+            }
+
+            $out[] = new Product([
+                'product_id' => $row->productId,
+                'upc' => $row->upc,
+                'brand' => $row->brand,
+                'name' => $row->description,
+                'item_id' => $row->items[0]->itemId,
+                'item_qty' => $row->items[0]->size,
+                'image_url' => $image,
+            ]);
+        }
+
+        return $out;
+    }
+}
--- app/TokenHelper.php
+++ app/TokenHelper.php
@@ -13,32 +13,8 @@ class TokenHelper {
      * Get a new token from the API
      */
     public static function GetToken() {
-        Log::info('Getting new access token.');
-
-        $clientId = env('KROGER_CLIENT_ID');
-        $secret = env('KROGER_SECRET');
-
-        if (!$clientId || !$secret) {
-            throw new RuntimeException('missing kroger API credentials');
-        }
-        $client = new Client([
-            'base_uri' => 'https://api.kroger.com/v1/',
-            'timeout'  => 2.0,
-        ]);
-
-        $resp = $client->post('connect/oauth2/token', [
-            'form_params' => [
-                'grant_type' => 'client_credentials'
-            ],
-            'auth' => [
-                $clientId,
-                $secret
-            ]
-        ]);
-
-        $data = json_decode($resp->getBody());
-
-        return($data->access_token);
+        $kroger = new Kroger();
+        return $kroger->GetAuthToken();
     }
 
     /**
