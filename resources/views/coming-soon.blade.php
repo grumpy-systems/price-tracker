b5a2a2390519308560e724b880bf2477d179b4a4
Grumpy <grumpy.systems@gmail.com>
2024-11-10 21:13:54 -0600

    Update coming soon styles

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -3,13 +3,14 @@
 @section('title', 'Do Eggs Cost More?')
 
 @section('content')
-    <div class="container">
-        <div class="row text-center">
-            <div class="col hero">
-                <h1>Coming Soon</h1>
+    <div class="hero">
+        <div class="container">
+            <div class="row align-items-center text-center">
+                <div class="col">
+                    <h1>Coming Soon...</h1>
+                </div>
             </div>
         </div>
+        <div class="overlay"></div>
     </div>
-
-    <div class="hero-divider"></div>
 @endsection
